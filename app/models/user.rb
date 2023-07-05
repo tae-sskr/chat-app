38b44900f0141027f0c8fbdf8ecadd1bb3c44872
tae-sskr <sasakura.tae@gmail.com>
2023-07-05 10:34:35 +0900

    roomテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, pressence: true
+  validates :name, presence: true
   
 end
